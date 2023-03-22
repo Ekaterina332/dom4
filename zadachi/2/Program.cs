af4e2da889d12157d1d59f732d79561e9a081ef5
Ekaterina332 <rina73.73@mail.ru>
2023-03-22 11:50:14 +0300

    2

---
+++ zadachi/2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Console.Clear();
+int[] array = new int[8];
+int n = 8;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+PrintArray(array);
