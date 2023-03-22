aa3930ddd968cc47f18b945bf3e6401fb60128b8
Ekaterina332 <rina73.73@mail.ru>
2023-03-22 11:07:14 +0300

    1

---
+++ zadachi/1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int f(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех чисел в цифре {number} = {f(number)}");
+
